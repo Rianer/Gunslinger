fe5833505d0ba5c6dfaf712528b2ec957ce32969
Rianer <aerhan2001@gmail.com>
2023-05-30 15:15:12 +0300

    Health bar mk1

--- Gunslinger_West_Hunt/Assets/Scripts/Player/Player.cs
+++ Gunslinger_West_Hunt/Assets/Scripts/Player/Player.cs
@@ -9,7 +9,6 @@ using UnityEngine.Rendering;
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(BoxCollider2D))]
 #endregion
 public class Player : MonoBehaviour
 {
