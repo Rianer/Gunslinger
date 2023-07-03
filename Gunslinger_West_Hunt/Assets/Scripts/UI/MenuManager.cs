c91948767d5478d6af9a6385168836f2621c590a
Adrian Erhan <75773955+Rianer@users.noreply.github.com>
2023-07-03 11:13:40 +0300

    Added Game Logo to Main Menu

--- Gunslinger_West_Hunt/Assets/Scripts/UI/MenuManager.cs
+++ Gunslinger_West_Hunt/Assets/Scripts/UI/MenuManager.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class MenuManager : MonoBehaviour
 {
     [SerializeField]List<MenuGroup> menuGroups = new List<MenuGroup>();
+    public MenuGroup gameLogoGroup;
     private GameObject activeMenu = null;
     public SoundManager soundManager;
+    private bool isInMainMenu = false;
     public static MenuManager Instance { get; private set; }
     private void Start()
     {
@@ -14,25 +16,32 @@ public class MenuManager : MonoBehaviour
         //{
         //    group.GetCurrentGroup().SetActive(false);
         //}
+        isInMainMenu = false;
+
         EnterMenu("Main Group");
         if(soundManager != null)
         {
             soundManager.PlaySound("main_menu_theme");
         }
         Instance = this;
+
     }
 
     public void EnterMenu(string menuName)
     {
         if(activeMenu != null)
         {
-            activeMenu.GetComponent<MenuGroup>().ExitAnimation();
+            //activeMenu.GetComponent<MenuGroup>().ExitAnimation();
+            LoadExitAnimation(activeMenu.GetComponent<MenuGroup>());
+            
         }
+        Debug.Log(isInMainMenu);
         foreach (var menuGroup in menuGroups)
         {
             if(menuGroup.GetCurrentGroup().name == menuName)
             {
                 activeMenu = menuGroup.GetCurrentGroup();
+                
                 LoadEnterAnimation(menuGroup);
             }
         }
@@ -49,11 +58,19 @@ public class MenuManager : MonoBehaviour
     public void LoadEnterAnimation(MenuGroup menuGroup)
     {
         menuGroup.EnterAnimation();
+        if(menuGroup.GetCurrentGroup().name == "Main Group")
+        {
+            gameLogoGroup.EnterAnimation();
+        }
     }
 
     public void LoadExitAnimation(MenuGroup menuGroup)
     {
         menuGroup.ExitAnimation();
+        if (menuGroup.GetCurrentGroup().name == "Main Group")
+        {
+            gameLogoGroup.ExitAnimation();
+        }
     }
 
 
